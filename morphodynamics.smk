1c022df23b1de80ff724698b2c8bb08b65f46af1
spectralnanodiamond <66789790+spectralnanodiamond@users.noreply.github.com>
2024-02-13 14:34:08 +0000

    running python from shell

--- morphodynamics.smk
+++ morphodynamics.smk
@@ -2,5 +2,10 @@ rule segment_with_stardist:
     input:
         "../data/301432316.tif"
     output:
-        "results/301432316.tif"
-    shell: "cp {input} {output}"
+        "results/1.npy"
+    shell:
+        "python scripts/change_format.py {input} {output}"
+
+
+
+#    shell: "cp {input} {output}"
---
+++ scripts/change_format.py
@@ -0,0 +1,13 @@
+#import skimage
+import numpy as np
+import sys
+print ('argument list', sys.argv)
+np.save('results/1.npy', np.array([1]))
+#myarray = 
+
+#def do_something(data_path, out_path, threads, myparam):
+    # python code
+#    return out_path
+
+#do_something(snakemake.input[0], snakemake.output[0], snakemake.threads, snakemake.config["myparam"])
+print(1)
